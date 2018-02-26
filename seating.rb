b454d15eca63170d64fc1cf692ab764d49008837
debbie <drosenfeld87@gmail.com>
2018-02-26 16:30:03 -0500

    start seating_plan exercise

---
+++ seating.rb
@@ -0,0 +1,8 @@
+seating_plan = [
+  [nil, "Pumpkin", nil, nil],
+  ["Socks", nil, "Mimi", nil],
+  ["Gismo", "Shadow", nil, nil],
+  ["Smokey","Toast","Pacha","Mau"]
+]
+
+def seating_plan.each_with_index

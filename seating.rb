b187694adfed1308a993bc34c65251425d93d653
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2018-02-26 17:53:35 -0500

    complete seating chart assignment

--- seating.rb
+++ seating.rb
@@ -5,8 +5,26 @@ seating_plan = [
   ["Smokey","Toast","Pacha","Mau"]
 ]
 
-
-def seating_plan.each_with_index do |seat|
-  puts "Row 1 seat 1 is free. Do you want to sit there? (y/n)"
-    answer =
+seating_plan.each_with_index do |row, row_num|
+  row.each_with_index do |seat, seat_num|
+    if seat == nil
+      puts "row #{row_num +1} seat #{seat_num +1} is free. Do you want to sit here? (y/n)"
+      answer = gets.chomp
+      if answer == "y"
+        puts "What is your name?"
+        name = gets.chomp
+        seating_plan[row_num][seat_num] = name
+      end
+    end
+  end
 end
+
+puts seating_plan
+
+# seating_plan.each_with_index do |row, row_num|
+#   row.each_with_index do |seat, seat_num|
+#     if seat == nil
+#       puts "row #{row_num +1} seat #{seat_num +1} is free"
+#     end
+#   end
+# end
